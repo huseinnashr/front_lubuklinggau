1a44c4325a31dfadbd2aaaf6417c685ab8ad765b
nvdhunter <nvdhunter007@gmail.com>
2018-01-16 17:38:34 +0700

    fixing 'cannot get' bug from express webserver

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(compression({ level: 9 }))
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', function(req, res) {
+  res.sendfile('./dist/index.html')
+})
 
 const port = process.env.PORT || '80';
 const host = process.env.HOST || '0.0.0.0';
